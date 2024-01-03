8a1dea9889b6bdcef0ff1af2c605aac8d4a87c80
Navial <victor.denis@gmail.com>
2024-01-03 18:22:06 +0100

    Style: using ant design

--- WEB3_revision/AntDesign/routed-anecdotes/src/components/createNew.jsx
+++ WEB3_revision/AntDesign/routed-anecdotes/src/components/createNew.jsx
@@ -1,6 +1,8 @@
 /* eslint-disable react/prop-types */
-import { useState } from "react";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { Input, Button, Form } from "antd";
+import { PlusCircleOutlined } from "@ant-design/icons";
 
 const CreateNew = (props) => {
   const [content, setContent] = useState("");
@@ -9,8 +11,7 @@ const CreateNew = (props) => {
 
   const navigate = useNavigate();
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
+  const handleSubmit = () => {
     props.addNew({
       content,
       author,
@@ -27,33 +28,65 @@ const CreateNew = (props) => {
   return (
     <div>
       <h2>create a new anecdote</h2>
-      <form onSubmit={handleSubmit}>
-        <div>
-          content
-          <input
+      <Form onFinish={handleSubmit}>
+        {/* <div>
+          <Input
+            placeholder="content"
             name="content"
             value={content}
             onChange={(e) => setContent(e.target.value)}
           />
-        </div>
-        <div>
-          author
-          <input
+        </div> */}
+        {/* <div> */}
+
+        <Form.Item
+          label="Content"
+          name="content"
+          rules={[{ required: true, message: "Please input some content !" }]}
+          value={content}
+          onChange={(e) => setContent(e.target.value)}
+        >
+          <Input />
+        </Form.Item>
+        <Form.Item
+          label="Author"
+          name="author"
+          rules={[{ required: true, message: "Please input the author !" }]}
+          value={author}
+          onChange={(e) => setAuthor(e.target.value)}
+        >
+          <Input />
+        </Form.Item>
+        {/* <Input
+            placeholder="author"
             name="author"
             value={author}
             onChange={(e) => setAuthor(e.target.value)}
           />
         </div>
-        <div>
-          url for more info
-          <input
+        <br/> */}
+        <Form.Item
+          label="Info"
+          name="info"
+          value={info}
+          onChange={(e) => setInfo(e.target.value)}
+        >
+          <Input />
+        </Form.Item>
+        {/* <div>
+          <Input
+            placeholder="info"
             name="info"
             value={info}
             onChange={(e) => setInfo(e.target.value)}
           />
-        </div>
-        <button>create</button>
-      </form>
+        </div> */}
+        <br />
+        <Button type="primary" htmlType="submit">
+          <PlusCircleOutlined />
+          Create
+        </Button>
+      </Form>
     </div>
   );
 };
